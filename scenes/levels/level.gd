19790c97473a6feb4a9d3b301fde110b0754b537
Teto <tetocoding@gmail.com>
2025-02-10 20:26:05 +0700

    coding interactivity

---
+++ scenes/levels/level.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var test_array: Array[String] = ["Test","Hello","Stuff"]
+
+func _ready() -> void:
+	$Logo.rotation_degrees = 90
+	
+	for i in test_array:
+		print(i)
+	
+	print(test_array[2])
+	
+func _process(delta: float) -> void:
+	$Logo.rotation_degrees += 5
+	
+	# flow -> if rotation > 180 -> 0
+	if $Logo.rotation_degrees > 180:
+		$Logo.rotation_degrees = 0
+	
+	if $Logo.position.x > 1000:
+		print("masok")
+		$Logo.pos.x = 0
