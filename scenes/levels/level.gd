d51e91521490b696438a4aeb7d9e8d7028048df9
Teto <tetocoding@gmail.com>
2025-02-11 15:32:58 +0700

    physics

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,15 +1,1 @@
 extends Node2D
-
-var test_array: Array[String] = ["Test","Hello","Stuff"]
-
-func _ready() -> void:
-	$Logo.rotation_degrees = 90
-	
-	for i in test_array:
-		print(i)
-	
-	print(test_array[2])
-	
-func _process(delta: float) -> void:
-	pass
-		
