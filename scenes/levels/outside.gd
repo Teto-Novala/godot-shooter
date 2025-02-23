eaee1add17cf0ea92ce459ed65765462d0aee78d
Teto <tetocoding@gmail.com>
2025-02-24 03:06:12 +0700

    add transition

--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -1,8 +1,6 @@
 extends LevelParent
 
-var inside_level_scene: PackedScene = preload("res://scenes/levels/inside.tscn")
-
 func _on_gate_player_entered_gate(_body: Variant) -> void:
 	var tween = create_tween()
 	tween.tween_property(player,"speed",0,0.5)
-	get_tree().change_scene_to_packed(inside_level_scene)
+	TransitionLayer.change_scene("res://scenes/levels/inside.tscn")
