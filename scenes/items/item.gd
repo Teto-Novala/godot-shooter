bb8ec8c34dc2fd6f90c709042aefcaa43ff21fca
Teto <tetocoding@gmail.com>
2025-02-25 12:35:54 +0700

    creating items

--- scenes/items/item.gd
+++ scenes/items/item.gd
@@ -7,6 +7,9 @@ var type = available_options[randi()%len(available_options)]
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
+var direction:Vector2
+var distance: int = randi_range(150,250)
+
 func _ready() -> void:
 	if type == 'laser':
 		sprite_2d.modulate = Color(0.1,0.2,0.8)
@@ -16,6 +19,13 @@ func _ready() -> void:
 		
 	if type == 'health':
 		sprite_2d.modulate = Color(0.1,0.8,0.1)
+		
+	# tween
+	var target_pos = position + direction * distance
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(self,"position",target_pos,0.5)
+	tween.tween_property(self,'scale',Vector2(1,1),0.3).from(Vector2(0,0))
 		
 
 func _process(delta: float) -> void:
--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -6,11 +6,22 @@ var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
 @onready var projectiles: Node2D = $Projectiles
 
 var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
+var item_scene: PackedScene = preload("res://scenes/items/item.tscn")
 
 @onready var camera_2d: Camera2D = $Player/Camera2D
 
 @onready var ui: CanvasLayer = $UI
 
+func _ready() -> void:
+	for container in get_tree().get_nodes_in_group('Container'):
+		container.connect("open",_on_container_opened)
+
+func _on_container_opened(pos,direction):
+	var item = item_scene.instantiate()
+	item.position = pos
+	item.direction = direction
+	$Items.call_deferred('add_child',item)
+	
 func _on_player_laser_signal_custom(pos,direction) -> void:
 	var laser = laser_scene.instantiate() as Area2D
 	laser.position = pos
